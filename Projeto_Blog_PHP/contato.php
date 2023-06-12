97b696472788e9f547315263c8136f72caa2c367
João Félix <felixjoao651@gmail.com>
2023-06-12 11:26:52 -0400

    Atualizando mobile

--- Projeto_Blog_PHP/contato.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once("templates/header.php");
-?>
- <h1>Página de contato</h1>
- <?php
-    include_once("templates/footer.php");
-?>
--- Projeto_Blog_PHP/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>Hora de codar &copy; 2020</p>
-</footer>
-</body>
-</html>
