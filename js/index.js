939ae40dfe39560feb90169c3001cb176269b0dd
João Félix <felixjoao651@gmail.com>
2023-06-20 15:54:05 -0400

    add efeitos javascript

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import navBar from './modules/scroll-navBar.js';
 import navBarScroll from './modules/btn-mobile.js';
+import initTabNav from './modules/initTabnav.js';
 
 initAnimacaoScroll();
 navBar();
 navBarScroll();
+initTabNav();
